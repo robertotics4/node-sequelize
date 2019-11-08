1fe266733de7ea7c3d9a6026ba98744437c62b8d
Roberto Oliveira <robertooliveiragp@gmail.com>
2019-11-08 18:59:12 -0300

    backend: estruturação finalizada

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,7 +8,11 @@ class User extends Model {
         }, {
             sequelize
         });
-    }  
+    }
+
+    static associate(models) {
+        this.hasMany(models.Address, { foreignKey: 'user_id', as: 'addresses' });
+    }
 }
 
 module.exports = User;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = express.Router();
-const UserController = require('./controllers/UserController');
 
-routes.post('/users', UserController.store);
+const UserController = require('./controllers/UserController');
+const AddressController = require('./controllers/AddressController');
+
 routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
+routes.get('/users/:user_id/addresses', AddressController.index);
+routes.post('/users/:user_id/addresses', AddressController.store);
 
 module.exports = routes;
